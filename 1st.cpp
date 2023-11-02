2953822a481e26e9dc16f7c09ab26f7833abca08
tharun716 <122478781+tharun716@users.noreply.github.com>
2023-11-02 12:44:42 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	pid_t pid=getpid();
+	pid_t parent_pid=getpid();
+	printf("current position ID:%d\n",pid);
+	printf("current process ID:%d\n",parent_pid);
+	return 0;
+}
